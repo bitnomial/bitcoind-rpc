e477a7028c9801f51e22ba290d451470a510c06e
Ian Shipman <ian.shipman@bitnomial.com>
2024-06-10 11:47:59 -0500

    test(nix): Updates multi-test
    - covered versions
    - improve output

--- nix/bitcoind-versions.nix
+++ nix/bitcoind-versions.nix
@@ -1,36 +1,14 @@
 {
-  v0-19-1 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-0.19.1/bitcoin-0.19.1-x86_64-linux-gnu.tar.gz";
-    sha256 = "032fz20d0nhc07gcharidmzbz3y871gk4i1lvv7yrh5nkz7pqxpq";
+  v25-2 = builtins.fetchTarball {
+    url = "https://bitcoincore.org/bin/bitcoin-core-25.2/bitcoin-25.2-x86_64-linux-gnu.tar.gz";
+    sha256 = "16a4lpvk19m6bmys4zlyh0q5wx83q7v2whjjdb32x0shgzpzvcmy";
   };
-
-  v0-20-0 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz";
-    sha256 = "02rizar9h2frip8m4lamdvjag18kb0blsnpckdy28mgvcck3z6ab";
+  v26-1 = builtins.fetchTarball {
+    url = "https://bitcoincore.org/bin/bitcoin-core-26.1/bitcoin-26.1-x86_64-linux-gnu.tar.gz";
+    sha256 = "1brxbss4xkhnh5fzp32dgwjwvz0y3y6sn9pa9w48s51p43m7n917";
   };
-
-  v0-20-1 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-x86_64-linux-gnu.tar.gz";
-    sha256 = "0rh4x2gvihgrsz7rb6z4iq6xsm7pmiyrrs03adyadfim6yac786h";
-  };
-
-  v0-21-0 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz";
-    sha256 = "1kfx4wbigrgiwx3s7609wp4g371b63r1hvij83h25i1j3dm4wijv";
-  };
-
-  v0-21-1 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-0.21.1/bitcoin-0.21.1-x86_64-linux-gnu.tar.gz";
-    sha256 = "05zn4dizi4kwbqid0vds90dwap6k6zrg8pv8qc7sxv2wg82klv9q";
-  };
-
-  v22-0 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz";
-    sha256 = "08l18bihda64bka4lhv046kdcjgwwngwws6v08lnrkp8j0l2l2rk";
-  };
-
-  v23-0 = builtins.fetchTarball {
-    url = "https://bitcoincore.org/bin/bitcoin-core-23.0/bitcoin-23.0-x86_64-linux-gnu.tar.gz";
-    sha256 = "01hf9h88sw06ppsx2pxshw6a4bd1j1yg4kgjrxdcr54gmwk891kn";
+  v27-0 = builtins.fetchTarball {
+    url = "https://bitcoincore.org/bin/bitcoin-core-27.0/bitcoin-27.0-x86_64-linux-gnu.tar.gz";
+    sha256 = "05i4zrdwr2rnbimf4fmklbm1mrvxg1bnv3yrrx44cp66ba0nd3jg";
   };
 }
